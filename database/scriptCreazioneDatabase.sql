7539b10ae5282f0c8a0d4c84145a0c1e761cd39d
Federico Calò <fedcal01@gmail.com>
2023-11-08 07:52:26 +0100

    Aggiunta caso d'uso e schema ER

---
+++ database/scriptCreazioneDatabase.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS sistemasanitario;
+USE sistemasanitario;
+CREATE TABLE ContattoRiferimento(idContatto INT PRIMARY KEY, nome VARCHAR(100),
+cognome VARCHAR(100), numeroCellulare VARCHAR(20));

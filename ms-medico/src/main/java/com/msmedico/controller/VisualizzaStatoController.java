0490b2c3cce7e745217d4a21cf10f057109e849e
Federico Calò <fedcal01@gmail.com>
2024-01-05 18:44:17 +0100

    VisualizzaStatoController ms-mmedico

--- ms-medico/src/main/java/com/msmedico/controller/VisualizzaStatoController.java
+++ ms-medico/src/main/java/com/msmedico/controller/VisualizzaStatoController.java
@@ -2,7 +2,11 @@ package com.msmedico.controller;
 
 import com.msmedico.constants.WebConstants;
 import com.msmedico.dto.MedicoDto;
+import com.msmedico.dto.params.visualizzastato.FindCartellaClinicaParams;
+import com.msmedico.dto.params.visualizzastato.FindOperazioneParams;
 import com.msmedico.dto.params.visualizzastato.FindStatoVisitaMedicaParams;
+import com.msmedico.dto.paziente.CartellaClinicaDto;
+import com.msmedico.dto.relationentities.OperazioneCartellaDto;
 import com.msmedico.dto.relationentities.VisitaMedicaCartellaDto;
 import com.msmedico.esito.EsitoMessaggiRequestContextHolder;
 import com.msmedico.esito.GenericResponseDto;
@@ -15,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,16 +33,44 @@ public class VisualizzaStatoController {
     @Autowired
     private VisualizzaStatoService visualizzaStatoService;
 
-    @Operation(summary = "Elenco di tutti i medici",
-            description = "Restituisce una lista di MediciDto che racchiude tutte le informazioni relaitive ai medici",
+    @Operation(summary = "Visualizzazione stato visite mediche filtrate per diversi campi",
+            description = "Restituisce una lista di VisitaMedicaCartellaDto che racchiude tutte le informazioni relaitive " +
+                    "alle visite mediche",
             operationId = "msMedicoGetStatoVisita")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Elenco di tutti i medici"),
             @ApiResponse(responseCode = "404", description = "Nessun elenco disponibile"),
             @ApiResponse(responseCode = "500", description = "Errore di sistema")
     })
-    @GetMapping(value ="/visita",produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value ="/visita-medica",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GenericResponseDto<List<VisitaMedicaCartellaDto>>> getStatoVisita(@ParameterObject FindStatoVisitaMedicaParams params){
         return ResponseEntity.ok(esitoMessaggiRequestContextHolder.buildGenericResponse(visualizzaStatoService.statoVisita(params)));
+    }
+    @Operation(summary = "Visualizzazione stato visite mediche filtrate per diversi campi",
+            description = "Restituisce una lista di CartellaClinicaDto che racchiude tutte le informazioni relaitive " +
+                    "alle cartelle cliniche",
+            operationId = "msMedicoGetCartelleCliniche")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Elenco di tutti i medici"),
+            @ApiResponse(responseCode = "404", description = "Nessun elenco disponibile"),
+            @ApiResponse(responseCode = "500", description = "Errore di sistema")
+    })
+    @GetMapping(value ="/cartelle-cliniche",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GenericResponseDto<List<CartellaClinicaDto>>> getCartelleCliniche(@ParameterObject FindCartellaClinicaParams params){
+        return ResponseEntity.ok(esitoMessaggiRequestContextHolder.buildGenericResponse(visualizzaStatoService.statoCartellaClinica(params)));
+    }
+
+    @Operation(summary = "Visualizzazione stato delle operazioni filtrate per diversi campi",
+            description = "Restituisce una lista di VisitaMedicaCartellaDto che racchiude tutte le informazioni relaitive " +
+                    "alle operazioni mediche",
+            operationId = "msMedicoGetStatoOperazione")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Elenco di tutte le operazioni filtrate"),
+            @ApiResponse(responseCode = "404", description = "Nessun elenco disponibile"),
+            @ApiResponse(responseCode = "500", description = "Errore di sistema")
+    })
+    @GetMapping(value ="/operazione",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GenericResponseDto<List<OperazioneCartellaDto>>> getStatoOperazione(@ParameterObject FindOperazioneParams params){
+        return ResponseEntity.ok(esitoMessaggiRequestContextHolder.buildGenericResponse(visualizzaStatoService.operazioneCartella(params)));
     }
 }
--- ms-medico/src/main/java/com/msmedico/dto/params/visualizzastato/FindCartellaClinicaParams.java
+++ ms-medico/src/main/java/com/msmedico/dto/params/visualizzastato/FindCartellaClinicaParams.java
@@ -1,7 +1,19 @@
 package com.msmedico.dto.params.visualizzastato;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class FindCartellaClinicaParams {
+    @Schema(description = "Id della cartella clinica da ricercare",type = "integer")
+    Integer idCartellaClinica;
+
+    @Schema(description = "Id del paziente",type = "integer")
+    Integer idPaziente;
+
+    @Schema(description = "Nome del paziente",type = "string")
+    String nome;
+
+    @Schema(description = "Cognome del paziente",type = "string")
+    String cognome;
 }
